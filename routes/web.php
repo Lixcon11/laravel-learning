6ee35273742d1ccedac56280b87571710b285c4a
Lixcon11 <106622009+Lixcon11@users.noreply.github.com>
2024-09-09 13:15:28 +0200

    Controller created

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ActivityController;
+
+Route::get("/activities", [ActivityController::class, "index"]);
 
 Route::get('/', function () {
     return view('welcome');
